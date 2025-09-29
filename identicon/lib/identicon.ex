01e215d1d54221417184bea05fb84157101c7bc2
Darren Whatford <darrenwhatford@gmail.com>
2025-09-29 08:19:28 +0100

    set up identicon

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -4,15 +4,27 @@ defmodule Identicon do
   """
 
   @doc """
-  Hello world.
+
 
   ## Examples
 
-      iex> Identicon.hello()
-      :world
-
   """
-  def hello do
-    :world
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
   end
+
+  def hash_input(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+  end
+
+  def pick_color([r, g, b | _tail] = hex) do
+    %{
+      color: {r, g, b},
+      hex: hex
+    }
+  end
+
 end
