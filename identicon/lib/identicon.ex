a224df907636e6eaffd2865b749b5b7a22795d79
Darren Whatford <darrenwhatford@gmail.com>
2025-09-29 17:58:14 +0100

    Lesson 43 identicon and image

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -7,11 +7,13 @@ defmodule Identicon do
 
 
   ## Examples
-
+      iex> Identicon.main("banana")
+      %Identicon.Image{color: {98, 97, 110}, hex: [98, 97, 110, 97, 110, 97, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
   """
   def main(input) do
     input
     |> hash_input
+    |> pick_color
   end
 
   def hash_input(input) do
@@ -19,6 +21,11 @@ defmodule Identicon do
     |> :binary.bin_to_list
 
     %Identicon.Image{hex: hex}
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    # IO.inspect image
+    %Identicon.Image{image | color: {r, g, b}} # r, g, b are the first three elements of the list and is a tuple.
   end
 
 
