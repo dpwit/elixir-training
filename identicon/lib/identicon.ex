79967aba8b2502aeffb2ad17d464b16ec67b1649
Darren Whatford <darrenwhatford@gmail.com>
2025-10-02 16:58:40 +0100

    Build pixel map 50 x 50

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -16,6 +16,7 @@ defmodule Identicon do
     |> pick_color
     |> build_grid
     |> fitler_odd_squares
+    |> build_pixel_map
   end
 
   def hash_input(input) do
@@ -63,6 +64,20 @@ defmodule Identicon do
     end)
 
     %Identicon.Image{image | grid: grid}
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map(grid, fn({_code, index}) ->
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
+
+      top_left = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
+
+      {top_left, bottom_right}
+    end)
+
+    %Identicon.Image{image | grid: grid, pixel_map: pixel_map}
   end
 
 end
